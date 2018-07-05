59b9b9ca94041db8019e7f94c6f5f2107d7c6f1b
bMik98 <bmik9898@gmail.com>
2018-07-06 00:27:17 +0300

    user interaction finished

--- src/main/java/ru/otus/spring/quiz/service/impl/ConsoleInteractionService.java
+++ src/main/java/ru/otus/spring/quiz/service/impl/ConsoleInteractionService.java
@@ -17,7 +17,7 @@ public class ConsoleInteractionService implements InteractionService {
             Scanner scanner = new Scanner(System.in);
             result = scanner.nextLine();
             if (result != null && !"".equals(result.trim())) {
-                confirmed = getConfirmation(String.format("Your name is %s? (y/n): ", result));
+                confirmed = getConfirmation(String.format("Your name is %s?", result));
             }
         }
         return result;
@@ -28,20 +28,29 @@ public class ConsoleInteractionService implements InteractionService {
         System.out.println("There are two kinds of questions: ");
         System.out.println(" 1. having one correct answer. In this case you have to select one option.");
         System.out.println(" 2. having the number of correct answers. To answer you have to select all of them.");
-        System.out.println("Type the id of the answer to select it or 'q' to finish and press Enter.");
+        System.out.println("Type the id of the answer and Enter to select it or nothing and Enter to finish.");
         System.out.println("So, lets begin!");
         questions.forEach(this::ask);
     }
 
     private void ask(Question question) {
         boolean multiOption = (question.numberOfRightAnswers() > 1);
-        showQuestionText(question, multiOption);
-        showPossibleAnswers(question);
-        makeSelection(question, multiOption);
+        boolean done = false;
+        while (!done) {
+            showQuestionText(question, multiOption);
+            showPossibleAnswers(question);
+            Integer selectedIndex = makeSelection(question, multiOption);
+            if (selectedIndex == null) {
+                done = true;
+            } else {
+                boolean currentSelection = question.getAnswers().get(selectedIndex).isSelected();
+                question.getAnswers().get(selectedIndex).setSelected(!currentSelection);
+            }
+        }
     }
 
     private void showQuestionText(Question question, boolean multiOption) {
-        System.out.println("--------------------");
+        System.out.println("--------------------------------------------------");
         System.out.println(question.getText());
         if (multiOption) {
             System.out.println("(select all right answers)");
@@ -53,37 +62,34 @@ public class ConsoleInteractionService implements InteractionService {
     private void showPossibleAnswers(Question question) {
         for (int i = 0; i < question.getAnswers().size(); i++) {
             Answer answer = question.getAnswers().get(i);
-            String selected = (answer.isSelected()) ? "(selected) " : "";
-            System.out.printf(" %d - %s%s %n", i, selected, answer.getText());
+            String selected = (answer.isSelected()) ? "[X]" : "[ ]";
+            System.out.printf("%s %2d - %s %n", selected, i, answer.getText());
         }
     }
 
-    private void makeSelection(Question question, boolean multiOption) {
-        int maxValue = question.getAnswers().size();
+    private Integer makeSelection(Question question, boolean multiOption) {
+        int maxValue = question.getAnswers().size() - 1;
         boolean done = false;
+        Integer result = null;
         while (!done) {
-            System.out.print("Your choice: ");
+            System.out.printf("Type the answer id (from 0 to %d) or nothing and press Enter: ", maxValue);
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             if (input != null && !"".equals(input.trim())) {
-                if ("q".equalsIgnoreCase(input)) {
-                    done = getConfirmation("Transmit your answer? (y/n): ");
-                } else {
-                    Integer selectedIndex = getIndexOfAnswer(input, maxValue);
-                    if (selectedIndex != null) {
-                        boolean currentSelection = question.getAnswers().get(selectedIndex).isSelected();
-                        question.getAnswers().get(selectedIndex).setSelected(!currentSelection);
-                    }
-                }
+                result = getIndexOfAnswer(input, maxValue);
+                done = (result != null);
+            } else {
+                done = getConfirmation("The answer to this question is completed?");
             }
         }
+        return result;
     }
 
     private Integer getIndexOfAnswer(String input, int maxValue) {
         Integer result;
         try {
             Integer value = Integer.valueOf(input);
-            result = (value > 0 && value <= maxValue) ? value : null;
+            result = (value < 0 || value > maxValue) ? null : value;
         } catch (NumberFormatException e) {
             result = null;
         }
@@ -91,9 +97,12 @@ public class ConsoleInteractionService implements InteractionService {
     }
 
     private boolean getConfirmation(String message) {
-        System.out.print(message);
+        System.out.print(message + " (y/n) [y]: ");
         Scanner scanner = new Scanner(System.in);
         String confirmation = scanner.nextLine();
+        if (confirmation == null || "".equals(confirmation)) {
+            return true;
+        }
         return "y".equalsIgnoreCase(confirmation);
     }
 }
